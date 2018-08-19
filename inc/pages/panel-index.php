2a25061b383a18dc1d88f014125087d710bc76cb
aljaxus <aljaxus.dev@gmail.com>
2018-08-19 10:11:19 +0200

    Added few buttons

--- inc/pages/panel-index.php
+++ inc/pages/panel-index.php
@@ -2,5 +2,10 @@
     <div class="card col s12 m8 l6 offset-s0 offset-m2 offset-l3 center-align">
         <h5>Welcome to SR panel</h5>
         <p>Chose a panel section from the navigation bar under "your profile"</p>
+		<div class="col s12 m12 l12 align-center">
+			<a class="btn waves-effect indigo" href="/panel/settings"><i class="material-icons left">settings</i>Account settings</a>
+			<a class="btn waves-effect red" href="/ajax/user.logout.php"><i class="material-icons left">exit_to_app</i>Logout</a>
+		</div>
+		<br> <br>
     </div>
 </div>
