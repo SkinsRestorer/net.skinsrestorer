c969d47ec6aeb8135433ffd17693f1559ce18966
aljaxus <aljaxus.dev@gmail.com>
2018-08-20 13:40:05 +0200

    Front end @ user settings

--- inc/pages/panel-settings.php
+++ inc/pages/panel-settings.php
@@ -5,17 +5,14 @@
 				<div class="input-field col s12">
 					<input id="pwdNow" type="password">
 					<label for="pwdNow">Current password</label>
-					<span class="helper-text" data-error="Rejected" data-success="Accepted"></span>
 				</div>
 				<div class="input-field col s12 m12 l6">
 					<input id="pwd1" type="password">
 					<label for="pwd1">New password</label>
-					<span class="helper-text" data-error="Rejected" data-success="Accepted"></span>
 				</div>
 				<div class="input-field col s12 m12 l6">
 					<input id="pwd2" type="password">
 					<label for="pwd2">Confirm new password</label>
-					<span class="helper-text" data-error="Rejected" data-success="Accepted"></span>
 				</div>
 				<div class="col s12 m12 l12 right-align">
 					<button class="waves-effect waves-light btn green" id="updatePwd-btn">Update password</button>
@@ -57,87 +54,76 @@ function updatePwd(){
 	let pwdNow = $('#pwdNow').val();
 	let pwd1 = $('#pwd1').val();
 	let pwd2 = $('#pwd2').val();
-	$.post(
-		'/ajax/user.settings.update.php',
-		{
-			pwdNow: pwdNow,
-			pwd1: pwd1,
-			pwd2: pwd2
-		}
-	).done(function( data ) {
-		// M.toast({html: '<i class="material-icons left">check</i> login triggered',activationPercent:0.7});
-  	}).fail(function( data ) {
-		M.toast({
-			html: '<i class="material-icons">warning</i> Unable to reach the server'
-		});
-  	}).always(function( data ) {
-		console.log("***************************************************");
-		console.log(data);
-		{
-			let pwdNow = $('#pwdNow').parent('div');
-			let pwd1 = $('#pwd1').parent('div');
-			let pwd2 = $('#pwd2').parent('div');
+	if (pwd1.length >= 5){
+		$.post(
+			'/ajax/user.settings.update.php',
 			{
-				let check1 = data.is_set;
-				let check2 = data.is_correct;
-
-				if (check1.pwdNow){
-					pwdNow.children('input').removeClass('invalid');
-					pwdNow.children('input').addClass('valid');
-					pwdNow.children('span.helper-text').attr('data-success', 'Current password is valid');
-				} else {
-					pwdNow.children('input').removeClass('valid');
-					pwdNow.children('input').addClass('invalid');
-					pwdNow.children('span.helper-text').attr('data-error', 'Current password is not valid');
-				}
-				if (check1.pwd1){
-					pwd1.children('input').removeClass('invalid');
-					pwd1.children('input').addClass('valid');
-					pwd1.children('span.helper-text').attr('data-success', 'New password is valid');
-				} else {
-					pwd1.children('input').removeClass('valid');
-					pwd1.children('input').addClass('invalid');
-					pwd1.children('span.helper-text').attr('data-error', 'New password is not valid');
-				}
-				if (check1.pwd2){
-					pwd2.children('input').removeClass('invalid');
-					pwd2.children('input').addClass('valid');
-					pwd2.children('span.helper-text').attr('data-success', 'New password confirmation is valid');
-				} else {
-					pwd2.children('input').removeClass('valid');
-					pwd2.children('input').addClass('invalid');
-					pwd2.children('span.helper-text').attr('data-error', 'New password confirmation is not valid');
-				}
-
-				if (data.is_updated){
-					pwd1.children('input').removeClass('invalid');
-					pwd2.children('input').removeClass('invalid');
-					pwd1.children('input').addClass('valid');
-					pwd2.children('input').addClass('valid');
-					pwd1.children('span.helper-text').attr('data-success', 'Password is matching');
-					pwd2.children('span.helper-text').attr('data-success', 'Password successfully changed');
-					{
-						M.toast({html: '<i class="material-icons left">check</i> Password successfully changed',activationPercent:0.7,classes:"green"});
-						let htmldata = '<div class="card-content"><div class="s12 center-align"><h4>Password successfully changed</h4><p>Please relog...</p></div></div>';
-						$('#updatePwdForm').html(htmldata);
-						setTimeout(function(){
-							window.location.replace(data.redirect);
-						}, 3000);
-					}
-				} else {
-					pwd1.children('input').addClass('invalid');
-					pwd2.children('input').addClass('invalid');
-					pwd1.children('input').removeClass('valid');
-					pwd2.children('input').removeClass('valid');
-					pwd1.children('span.helper-text').attr('data-success', 'Password was not changed');
-					pwd2.children('span.helper-text').attr('data-success', 'Password was not changed');
-					M.toast({html: 'Password was not changed',activationPercent:0.7,classes:"red"});
-				}
-
+				pwdNow: pwdNow,
+				pwd1: pwd1,
+				pwd2: pwd2,
+				update: 'password'
 			}
-		}
+		).done(function( data ) {
+			// M.toast({html: '<i class="material-icons left">check</i> login triggered',activationPercent:0.7});
+		}).fail(function( data ) {
+			M.toast({
+				html: '<i class="material-icons">warning</i> Unable to reach the server'
+			});
+		}).always(function( data ) {
+			console.log("***************************************************");
+			console.log(data);
+			{
+				let pwdNow = $('#pwdNow');
+				let pwd1 = $('#pwd1');
+				let pwd2 = $('#pwd2');
+				{
+					let check = data.debug;
 
-	});
+					if (check.pwdNow){
+						pwdNow.removeClass('invalid').addClass('valid');
+					} else {
+						pwdNow.removeClass('valid').addClass('invalid');
+					}
+					if (check.pwd1){
+						pwd1.removeClass('invalid').addClass('valid');
+					} else {
+						pwd1.removeClass('valid').addClass('invalid');
+					}
+					if (check.pwd2){
+						pwd2.removeClass('invalid').addClass('valid');
+					} else {
+						pwd2.removeClass('valid').addClass('invalid');
+					}
+
+					if (data.is_success.dbupdate_newpwd){
+						pwdNow.removeClass('invalid').addClass('valid');
+						pwd1.removeClass('invalid').addClass('valid');
+						pwd2.removeClass('invalid').addClass('valid');
+						{
+							M.toast({html: '<i class="material-icons left">check</i> Password successfully changed',activationPercent:0.7,classes:"green"});
+							let htmldata = '<div class="card-content"><div class="s12 center-align"><h4>Password successfully changed</h4><p>Please relog...</p></div></div>';
+							$('#updatePwdForm').html(htmldata);
+							setTimeout(function(){
+								window.location.replace(data.redirect);
+							}, 3000);
+						}
+					} else {
+						if (check.pwdNow){
+							M.toast({html: 'Currect password is not correct',activationPercent:0.7,classes:"red"});
+						} else {
+							M.toast({html: 'Currect password is not correct',activationPercent:0.7,classes:"red"});
+						}
+						pwdNow.addClass('invalid').removeClass('valid');
+						pwd1.addClass('invalid').removeClass('valid');
+						pwd2.addClass('invalid').removeClass('valid');
+					}
+
+				}
+			}
+		});
+	} else {
+		M.toast({html: '<i class="material-icons left">warning</i> Password must have 5+ characters'});
+	}
 
 }
 
